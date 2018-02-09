13b1f956badc29971247e981a59a8f97fbc86e68
iRogue32 <mckee.max32@gmail.com>
2018-02-09 13:51:35 -0800

    added hash functions

--- ChampionPointRespec.lua
+++ ChampionPointRespec.lua
@@ -150,6 +150,79 @@ ChampionPointRespec.Defaults = {
 	
 }
 
+local base64chars = {[0]='A',[1]='B',[2]='C',[3]='D',[4]='E',[5]='F',[6]='G',[7]='H',[8]='I',[9]='J',[10]='K',[11]='L',[12]='M',[13]='N',[14]='O',[15]='P',[16]='Q',[17]='R',[18]='S',[19]='T',[20]='U',[21]='V',[22]='W',[23]='X',[24]='Y',[25]='Z',[26]='a',[27]='b',[28]='c',[29]='d',[30]='e',[31]='f',[32]='g',[33]='h',[34]='i',[35]='j',[36]='k',[37]='l',[38]='m',[39]='n',[40]='o',[41]='p',[42]='q',[43]='r',[44]='s',[45]='t',[46]='u',[47]='v',[48]='w',[49]='x',[50]='y',[51]='z',[52]='0',[53]='1',[54]='2',[55]='3',[56]='4',[57]='5',[58]='6',[59]='7',[60]='8',[61]='9',[62]='-',[63]='_'}
+
+local function GetBase64(input)
+	if(input) < 64 then
+		return "A"..base64chars[input]
+	else
+		local first = math.floor(input / 64)
+		first = base64chars[first]
+		second = input - 64
+		second = base64chars[second]
+		return first..second
+	end
+end
+
+local base64bytes = {['A']=0,['B']=1,['C']=2,['D']=3,['E']=4,['F']=5,['G']=6,['H']=7,['I']=8,['J']=9,['K']=10,['L']=11,['M']=12,['N']=13,['O']=14,['P']=15,['Q']=16,['R']=17,['S']=18,['T']=19,['U']=20,['V']=21,['W']=22,['X']=23,['Y']=24,['Z']=25,['a']=26,['b']=27,['c']=28,['d']=29,['e']=30,['f']=31,['g']=32,['h']=33,['i']=34,['j']=35,['k']=36,['l']=37,['m']=38,['n']=39,['o']=40,['p']=41,['q']=42,['r']=43,['s']=44,['t']=45,['u']=46,['v']=47,['w']=48,['x']=49,['y']=50,['z']=51,['0']=52,['1']=53,['2']=54,['3']=55,['4']=56,['5']=57,['6']=58,['7']=59,['8']=60,['9']=61,['-']=62,['_']=63,['=']=nil}
+
+local function getBase10(input)
+	local first = string.sub(input, 1, 1)
+	first = base64bytes[first]
+	local second = string.sub(input, 2, 2)
+	second = base64bytes[second]
+	d(first + second)
+	return first + second
+end
+
+local function createCPHash(input)
+	local hash = ""
+	for discipline = 1, GetNumChampionDisciplines() do
+		for skill = 1, GetNumChampionDisciplineSkills() - 4 do																								
+			hash = hash..GetBase64(sv[selected][discipline][skill])
+		end
+	end
+	d(hash)
+	return hash
+	
+	--local cpSkill = GetNumPointsSpentOnChampionSkill(8, 3)
+	--cpSkill = GetBase64(cpSkill)
+	--d(cpSkill)
+end
+
+local function closeHashWindow()
+	window:SetHidden(true)
+end
+
+local function openHashWindow() 
+	edit:SetText("")
+	window:SetHidden(false)
+	local hash = createCPHash()
+	edit:SetText(hash)
+end
+
+local function createHashWindow()
+	window = WINDOW_MANAGER:CreateTopLevelWindow("hashWindow")
+	window:SetAnchor(CENTER, GuiRoot, CENTER, 0, 0)
+	window:SetHidden(false)
+	window:SetDrawLayer(1)
+	window:SetClampedToScreen(true)
+	window:SetMovable(false)
+	window:SetMouseEnabled(true)
+	window:SetDimensions(400, 450)
+	window:SetHidden(true)
+	local windowBackground = WINDOW_MANAGER:CreateControlFromVirtual("hashWindowBackground", hashWindow, "ZO_DefaultBackdrop")
+	local EditBackdrop = WINDOW_MANAGER:CreateControlFromVirtual("hashWindowEditBackdrop", hashWindow, "ZO_MultiLineEditBackdrop_Keyboard")
+	EditBackdrop:SetDimensions(350, 250)
+	EditBackdrop:SetAnchor(CENTER, hashWindow, CENTER, 0, 20)
+	edit = WINDOW_MANAGER:CreateControlFromVirtual("hashWindowEditBackdropEdit", hashWindowEditBackdrop, "ZO_DefaultEditMultiLineForBackdrop")
+	local title = WINDOW_MANAGER:CreateControlFromVirtual("hashWindowTitle", hashWindow, "ZO_WindowTitle")
+	title:SetText("CP HASH")
+	title:SetAnchor(TOP, hashWindow, TOP, 0, 10)
+	local cancelButton = WINDOW_MANAGER:CreateControlFromVirtual("hashWindowCloseButton", hashWindow, "SavingEditBoxCancelButton")
+	cancelButton:SetAnchor(BOTTOM, hashWindow, BOTTOM, 0, 0)
+end
+
 local function savePositions(self)
 	--d("saving positions...")
 	local valid, point, _, relPoint, offsetX, offsetY = self:GetAnchor(0)
@@ -395,6 +468,8 @@ function ChampionPointRespec:createWindow()
 	m_comboBox:UpdateItems()
 	m_comboBox:SelectFirstItem()
 	
+	createHashWindow()
+	
 end
 
 
@@ -418,6 +493,10 @@ function ChampionPointRespec:Initialize()
 	--SLASH_COMMANDS["/setcpconfigascurrent"] = setCPBuild
 	--SLASH_COMMANDS["/championpointrespec"] = UI
 	--SLASH_COMMANDS["/cpconfigrename"] = cprename
+	SLASH_COMMANDS["/cphash"] = createCPHash
+	SLASH_COMMANDS["/openhashwindow"] = openHashWindow
+	SLASH_COMMANDS["/closehashwindow"] = closeHashWindow	
+	SLASH_COMMANDS["/getbaseten"] = getBase10
 	
 end
 
